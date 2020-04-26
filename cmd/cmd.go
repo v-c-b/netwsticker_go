e70ec1f91a444814ebf8b29490da1cf67ea86c7e
Volker Bergner <volker@bergner.one>
2020-04-26 13:32:36 +0200

    CLI test v5

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -9,8 +9,7 @@ import (
 var rootCmd = &cobra.Command{
   Use: "newsticker_go",
   Short: "Read Atom feeds",
-  Long: 'Just a small CLI application.
-        Read Atom feeds'
+  Long: 'Just a small CLI application. Read Atom feeds'
 }
 
 func Exec() {
