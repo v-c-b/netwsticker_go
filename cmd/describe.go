378d96f9be0927edf56b479b6a26d5e0d36871dd
Volker Bergner <volker@bergner.one>
2020-04-26 14:10:12 +0200

    Describe function built into CLI

---
+++ cmd/describe.go
@@ -0,0 +1,19 @@
+package cmd
+import (
+  "fmt"
+  "github.com/mmcdole/gofeed"
+)
+
+func describe(id string) {
+  fp := gofeed.NewParser()
+  feed, _ := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
+  for _, element := range feed.Items {
+    if element.GUID == id {
+      fmt.Println(element.Title)
+        fmt.Println(" ")
+      fmt.Println(element.Description)
+      return
+    }
+  }
+  fmt.Println(id + " not found.")
+}
--- cmd/list.go
+++ cmd/list.go
@@ -8,6 +8,6 @@ func list() {
   fp := gofeed.NewParser()
   feed, _ := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
   for i :=0; i < 5; i++ {
-    fmt.Println(feed.Items[i].Title)
+    fmt.Println(feed.Items[i].Title + ":" + feed.Items[i].GUID)
   }
 }
